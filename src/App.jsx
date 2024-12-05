453718ae56df269b5bf72fcde915b124748cf637
Joseph Albert Garganera <josephalbertgarganera@Josephs-MacBook-Pro.local>
2024-12-05 13:11:28 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="x1:padding-1 wide:padding-r padding-b">Hero</section>
+    <section></section>
+  </main>
+);
+
+export default App;
